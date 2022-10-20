ec202f1386949539dbb401d8be988e26deb4deca
Jerry Dai <jdai@lcs.on.ca>
2022-10-20 12:14:53 -0400

    Got a single pass of  array done and got one number to the end.

--- BubbleSortImplementation/main.swift
+++ BubbleSortImplementation/main.swift
@@ -52,4 +52,22 @@ print("Unsorted:")
 print(dataSet)
 waitForUserInput()
 
+//One path through the array to float the highest number to the end
+for j in 0..<dataSet.count - 1 {
+    
+    //compare left value to right value
+    if dataSet[j] > dataSet[j+1] {
+        //swap values (when left value is more than right value)
+        let temporaryValue = dataSet[j] //Set aside the left value
+        dataSet[j] = dataSet[j + 1]  //Response the left with right
+        dataSet[j+1] = temporaryValue // Replacing right with the temporaryValue
+        
+        
+    }
+    
+}
 
+//Print the array after the pass
+print("Array after pass:")
+print(dataSet)
+waitForUserInput()
