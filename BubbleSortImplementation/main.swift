41c69d3a21c4ddca9110877f9db029688f40e6fd
Jerry Dai <jdai@lcs.on.ca>
2022-10-20 11:42:41 -0400

    make the list and added three values

--- BubbleSortImplementation/main.swift
+++ BubbleSortImplementation/main.swift
@@ -1,4 +1,38 @@
 import Foundation
 
-print("Hello World")
+//func bubbleSort(unsortedList: [Int]) -> [Int]
+//{
+//    var sortedList = unsortedList
+//
+//    for _ in 0..<sortedList.count
+//    {
+//        for i in 0..<(sortedList.count - 1)
+//        {
+//            if(sortedList[i] > sortedList[i + 1])
+//            {
+//                // swap list
+//                let placeholder = sortedList[i]
+//                sortedList[i] = sortedList[i + 1]
+//                sortedList[i + 1] = placeholder
+//            }
+//        }
+//
+//    }
+//
+//    return sortedList;
+//}
+//
+//var unsortedList = [1, 2, 1239, 534, 1234, 590, 10]
+//print("unsorted list \(unsortedList)")
+//var sortedList = bubbleSort(unsortedList: unsortedList)
+//
+//print("sorted list \(sortedList)")
+
+var dataSet: [Int] = []
+
+
+dataSet.append(18)
+dataSet.append(36)
+dataSet.append(9)
+
 
